ae334a80f3d158d33c87f7ea5481cc440d1a044a
kabosusang <375487345@qq.com>
2025-05-29 23:57:10 +0800

    DrawLine

--- include/Rasterization/Rasterization.hpp
+++ include/Rasterization/Rasterization.hpp
@@ -1,7 +1,15 @@
 #pragma once
+#include "Tools/Vector.hpp"
 #include <Tools/Color.hpp>
+#include <base/Painter.hpp>
 
-class Rasterization
-{
+class Rasterization {
+public:
+	Rasterization();
+	void Renderer(float time);
 
+private:
+	void DrawLine(Vec2 P0, Vec2 P1, Color);
+
+	Painter& painter;
 };
--- src/Rasterization/Rasterization.cpp
+++ src/Rasterization/Rasterization.cpp
@@ -1,1 +1,29 @@
+#include "base/Painter.hpp"
 #include <Rasterization/Rasterization.hpp>
+
+Rasterization::Rasterization()
+:painter(Painter::getInstance())
+{
+
+}
+
+
+void Rasterization::Renderer(float time)
+{
+    painter.Clear(Color{ 255, 255, 255, 255 });
+    
+
+}
+
+void Rasterization::DrawLine(Vec2 P0,Vec2 P1,Color color)
+{
+    //斜率
+    auto a = (P1.y - P0.y) / (P1.x - P0.x);
+    auto b = P0.y - a * P0.x;
+    for (auto x = P0.x; x < P1.x; x++)
+    {   
+        auto y = a * x + b;
+        painter.PutPixel(x, y, color);
+    }
+
+}
